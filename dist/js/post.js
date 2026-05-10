e751335e52d5f295962408835d659e0f371e24c5
Aleksander Heintz <alxandr@alxandr.me>
2026-05-10 14:18:04 +0200

    fix: allow post action to fail (#40)

--- dist/js/post.js
+++ dist/js/post.js
@@ -1,13 +1,14 @@
 import {
   debug,
-  setFailed,
-  upload
-} from "./main-sw8kd3a5.js";
+  upload,
+  warning
+} from "./main-1xwkbz70.js";
 
 // src/post.mts
 try {
-  upload();
+  await upload();
   debug("Upload done");
 } catch (error) {
-  setFailed(`Action failed with error: ${error}`);
+  warning(`Post action failed, but not failing build`);
+  warning(`Post action failed with error: ${error}`);
 }
